deadd230727e283a48119ac0486ac4f3e3884a60
Divine Orji <dpkreativ@gmail.com>
2021-06-24 10:20:50 +0100

    change from double quotes to backticks

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -2,7 +2,7 @@ const client = require("../db/db_config");
 
 // Get all courses
 const getCourses = async (req, res) => {
-  const allCourses = await client.query("SELECT * FROM mydb.courses");
+  const allCourses = await client.query(`SELECT * FROM mydb.courses`);
 
   res.send({ allCourses });
 };
