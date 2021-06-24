85779b5bfb78bc54fb462b690e2a5e4873530a14
Divine Orji <dpkreativ@gmail.com>
2021-06-24 09:46:01 +0100

    create controller to add new course

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -7,7 +7,38 @@ const getCourses = async (req, res) => {
   res.send({ allCourses });
 };
 
+const addCourse = async (req, res) => {
+  const allProperties = Object.keys(req.body).length;
+
+  if (allProperties !== 4) {
+    res.code(400).send({
+      error:
+        "Some properties are missing! Add the name, description, author, and link.",
+    });
+    return;
+  }
+
+  try {
+    const newCourse = await client.insert({
+      table: "courses",
+      records: [
+        {
+          name: req.body.name,
+          description: req.body.description,
+          author: req.body.author,
+          link: req.body.link,
+        },
+      ],
+    });
+
+    res.send({ newCourse });
+  } catch (error) {
+    res.send({ error });
+  }
+};
+
 // Export controllers
 module.exports = {
   getCourses,
+  addCourse,
 };
